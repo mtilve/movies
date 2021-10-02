4f8f7e8a615e04a19857a0321fddfe6afdce59ca
Marcelo <marcelo.tilve@gmail.com>
2021-10-02 20:11:20 -0300

    init

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env'});
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {});
+        console.log("Connected DB !!!");
+    }catch(error){
+        console.log("Error: " + error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
